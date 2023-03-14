d361dd593515ca410e38fb5f3a8a4335f7bbc90d
JohnMarcSevillana <john_marc_sevillana@dlsu.edu.ph>
2023-03-14 15:09:00 +0800

    fix validation for register
    validations errors is adjusted

--- STSWENG-Website/validators.js
+++ STSWENG-Website/validators.js
@@ -1,9 +1,15 @@
 const { body } = require('express-validator');
 
 const registerValidation = [
-    body('regUsername').not().isEmpty().withMessage("Username is required!"),
-    body('regPassword').not().isEmpty().withMessage("Password is required!"),
-    body('regRePassword').not().isEmpty().withMessage("Please retype password!")
+    body('username').not().isEmpty().withMessage("Username is required!"),
+    body('password').isLength({ min: 8 }).withMessage("Password must be at least 8 characters long."),
+    body('rePassword').isLength({ min: 8 }).withMessage("Re-enter password must be at least 8 characters long.")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords must match.");
+      }
+      return true;
+    })
 ];
 
 const loginValidation = [
