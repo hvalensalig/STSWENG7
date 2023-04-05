be0ecc2061ce23770d6a04c5713bf3438275141d
JohnMarcSevillana <john_marc_sevillana@dlsu.edu.ph>
2023-04-05 18:41:59 +0800

    Manually return reverted file
    Reverting the reverted file manually

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
-const { registerValidation, loginValidation } = require('../validators.js')
+const { registerValidation, loginValidation } = require('../validators.js');
+const { isPublic, isPrivate } = require('../middlewares/userAuth');
 
 router.post('/register', isPublic, registerValidation, userController.register)
 router.post('/login', isPublic, loginValidation, userController.login);
