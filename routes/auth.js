6490e02e1c65f9b7e1b0239d9fa49f4f18f3b38b
JohnMarcSevillana <john_marc_sevillana@dlsu.edu.ph>
2023-04-05 14:32:24 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const userController = require('../controllers/userController');
 const { registerValidation, loginValidation } = require('../validators.js');
 const { isPublic, isPrivate } = require('../middlewares/userAuth');
 
-router.post('/register', registerValidation, userController.register)
-router.post('/login', loginValidation, userController.login)
+//router.post('/register', isPublic, registerValidation, userController.register)
+router.post('/login', isPublic, loginValidation, userController.login);
 //router.get('/logout', userController.logoutUser)
 router.get(`/logout`, isPrivate, userController.logoutUser);
 
